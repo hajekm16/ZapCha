acf6a258b5fa3dd3b384b4f32c60eeb7c184b8b9
Mata <mucachoapps@gmail.com>
2023-01-21 16:27:52 +0100

    akce s databazi courutines, livedata a tlacitka

--- app/src/main/java/mucacho/apps/zapcha/ProductViewModel.kt
+++ app/src/main/java/mucacho/apps/zapcha/ProductViewModel.kt
@@ -1,18 +1,30 @@
 package mucacho.apps.zapcha
 
 import android.app.Application
+import android.provider.SyncStateContract.Helpers.insert
 import android.view.ViewDebug.IntToString
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import mucacho.apps.zapcha.database.ZapChaProduct
 import mucacho.apps.zapcha.database.ZapchaDatabase
 import mucacho.apps.zapcha.database.ZapchaDatabaseDao
 
 class ProductViewModel(
     val database: ZapchaDatabaseDao,
     application: Application) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var currentProduct = MutableLiveData<ZapChaProduct?>()
+
+    private val products = database.getAllProducts()
+
     var name = ""
     var price = 0
     var descr = ""
@@ -20,9 +32,64 @@ class ProductViewModel(
     val stock : LiveData<Int>
         get() = _stock
     val stockString = Transformations.map(stock,{onStock -> stockToString(onStock)})
+
     init{
 //        one time setting
         _stock.value = 0
+        initializeCurrentProduct()
+    }
+
+    private fun initializeCurrentProduct() {
+        uiScope.launch {
+            currentProduct.value = getCurrentProductFromDatabase()
+        }
+    }
+
+    private suspend fun getCurrentProductFromDatabase(): ZapChaProduct? {
+        return withContext(Dispatchers.IO) {
+            var product = database.getLastProduct()
+            product
+        }
+    }
+
+    fun onSaveProduct(){
+        uiScope.launch {
+            val newProduct = ZapChaProduct()
+            insert(newProduct)
+            currentProduct.value = getCurrentProductFromDatabase()
+        }
+    }
+
+    private suspend fun insert(product: ZapChaProduct){
+        withContext(Dispatchers.IO){
+            database.insert(product)
+        }
+    }
+
+    fun onDeleteProduct(){
+        uiScope.launch {
+            val oldProduct = currentProduct.value ?: return@launch
+            delete(oldProduct)
+        }
+    }
+
+    private suspend fun delete(product: ZapChaProduct) {
+        withContext(Dispatchers.IO) {
+            database.delete(product)
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            currentProduct.value = null
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
     }
 
     fun stockToString(qty : Int) : String{
@@ -57,5 +124,10 @@ class ProductViewModel(
                 descr = "Tohle je porce antioxidantu..."
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
