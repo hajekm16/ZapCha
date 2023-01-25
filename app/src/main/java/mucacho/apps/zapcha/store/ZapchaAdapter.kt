96d6c3314d3983d72f29b779f55f690ec5ce758b
Mata <mucachoapps@gmail.com>
2023-01-25 16:02:12 +0100

    vytvoreni vlastniho view pro adapter

--- app/src/main/java/mucacho/apps/zapcha/store/ZapchaAdapter.kt
+++ app/src/main/java/mucacho/apps/zapcha/store/ZapchaAdapter.kt
@@ -1,16 +1,21 @@
 package mucacho.apps.zapcha.store
 
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import mucacho.apps.zapcha.R
 import mucacho.apps.zapcha.database.ZapChaProduct
 
-class ZapchaAdapter : RecyclerView.Adapter<ZapchaAdapter.TextItemViewHolder>() {
+class ZapchaAdapter : RecyclerView.Adapter<ZapchaAdapter.ViewHolder>() {
 
-    class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView){
-
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val productName: TextView = itemView.findViewById(R.id.product_name)
+        val productStock: TextView = itemView.findViewById(R.id.product_stock)
+        val productImage: ImageView = itemView.findViewById(R.id.product_image)
     }
 
     var data = listOf<ZapChaProduct>()
@@ -21,17 +26,19 @@ class ZapchaAdapter : RecyclerView.Adapter<ZapchaAdapter.TextItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.productName
+        holder.productName.text = item.productName
+        holder.productStock.text = item.productStock.toString()
+        holder.productImage.setImageResource(R.drawable.zapcha)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
-            .inflate(R.layout.text_product_view, parent, false) as TextView
+            .inflate(R.layout.list_product_zapcha, parent, false)
 
-        return TextItemViewHolder(view)
+        return ViewHolder(view)
     }
 }
