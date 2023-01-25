215950d384c5e6e75b556da5be6c5fe0676fb198
Mata <mucachoapps@gmail.com>
2023-01-25 20:21:09 +0100

    encapsulation, separation of concerns, smaller icons

--- app/src/main/java/mucacho/apps/zapcha/store/ZapchaAdapter.kt
+++ app/src/main/java/mucacho/apps/zapcha/store/ZapchaAdapter.kt
@@ -12,15 +12,32 @@ import mucacho.apps.zapcha.database.ZapChaProduct
 
 class ZapchaAdapter : RecyclerView.Adapter<ZapchaAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
         val productName: TextView = itemView.findViewById(R.id.product_name)
         val productStock: TextView = itemView.findViewById(R.id.product_stock)
         val productImage: ImageView = itemView.findViewById(R.id.product_image)
+
+        fun bind(item : ZapChaProduct) {
+            productName.text = item.productName
+            productStock.text = item.productStock.toString()
+            productImage.setImageResource(R.drawable.zapcha)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup) :ViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.list_product_zapcha, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
     }
 
     var data = listOf<ZapChaProduct>()
         set(value) {
             field = value
+//            prekresli veskere polozky
             notifyDataSetChanged()
         }
 
@@ -28,17 +45,11 @@ class ZapchaAdapter : RecyclerView.Adapter<ZapchaAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.productName.text = item.productName
-        holder.productStock.text = item.productStock.toString()
-        holder.productImage.setImageResource(R.drawable.zapcha)
+
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.list_product_zapcha, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 }
