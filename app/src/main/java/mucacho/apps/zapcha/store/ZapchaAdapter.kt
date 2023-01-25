0d457659a55a047bc72e68b0acaa1c6498bee87c
Mata <mucachoapps@gmail.com>
2023-01-25 20:57:11 +0100

    binding in Adapter, refactor(inline)

--- app/src/main/java/mucacho/apps/zapcha/store/ZapchaAdapter.kt
+++ app/src/main/java/mucacho/apps/zapcha/store/ZapchaAdapter.kt
@@ -1,37 +1,30 @@
 package mucacho.apps.zapcha.store
 
-import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import mucacho.apps.zapcha.R
 import mucacho.apps.zapcha.database.ZapChaProduct
+import mucacho.apps.zapcha.databinding.ListProductZapchaBinding
 
 class ZapchaAdapter : ListAdapter<ZapChaProduct, ZapchaAdapter.ViewHolder>(ZapchaProductDiffCallback()) {
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val productName: TextView = itemView.findViewById(R.id.product_name)
-        val productStock: TextView = itemView.findViewById(R.id.product_stock)
-        val productImage: ImageView = itemView.findViewById(R.id.product_image)
+    class ViewHolder private constructor(val binding: ListProductZapchaBinding) : RecyclerView.ViewHolder(binding.root){
 
-        fun bind(item : ZapChaProduct) {
-            productName.text = item.productName
-            productStock.text = item.productStock.toString()
-            productImage.setImageResource(R.drawable.zapcha)
+        fun bind(item: ZapChaProduct) {
+            binding.productName.text = item.productName
+            binding.productStock.text = item.productStock.toString()
+            binding.productImage.setImageResource(R.drawable.zapcha)
         }
 
         companion object {
             fun from(parent: ViewGroup) :ViewHolder{
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                    .inflate(R.layout.list_product_zapcha, parent, false)
+                val binding = ListProductZapchaBinding.inflate(layoutInflater, parent, false)
 
-                return ViewHolder(view)
+                return ViewHolder(binding)
             }
         }
     }
